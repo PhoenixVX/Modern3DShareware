45ae5e87a1926882a994896ca630aad3b8ba9abc
PheonixVX <TheGrimReaper.VX@gmail.com>
2021-03-14 12:50:40 -0400

    Change Barrel BER to work

--- src/main/java/io/github/PheonixVX/ThreeDShareware/registry/entity/CustomBarrelBlockEntityRenderer.java
+++ src/main/java/io/github/PheonixVX/ThreeDShareware/registry/entity/CustomBarrelBlockEntityRenderer.java
@@ -17,9 +17,11 @@ import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.Entity;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Vec2f;
 
 import java.util.function.Function;
@@ -32,8 +34,8 @@ public class CustomBarrelBlockEntityRenderer extends BlockEntityRenderer<BarrelB
 
 	@Override
 	public void render (BarrelBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-		GlStateManager.pushMatrix();
-		GlStateManager.translatef( 0.5F, 0,0.5F);
+		matrices.push();
+		matrices.translate( 0.5F, 0,0.5F);
 		this.dispatcher.textureManager.bindTexture(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE);
 		MinecraftClient client = MinecraftClient.getInstance();
 		Entity cameraEntity = client.getCameraEntity();
@@ -42,26 +44,25 @@ public class CustomBarrelBlockEntityRenderer extends BlockEntityRenderer<BarrelB
 			ZERO = cameraEntity.getRotationClient();
 		}
 
-		GlStateManager.rotatef(-ZERO.y, 0.0F, 1.0F, 0.0F);
-		GlStateManager.rotatef(180.0F, 0.0F, 0.0F, 1.0F);
+		matrices.multiply(new Vector3f(0, 1, 0).getDegreesQuaternion(-ZERO.y));
+		matrices.multiply(new Vector3f(1.0F,0.0F,0.0F).getDegreesQuaternion(180.0F));
 		Sprite barrelSprite = client.getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).apply(new Identifier("block/barrel_side"));
 		Sprite fireSprite = client.getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).apply(new Identifier("block/fire_0"));
-		//Sprite barrelSprite = (Sprite) client.getSpriteAtlas(new Identifier("block/barrel_side"));
-		//Sprite fireSprite = (Sprite) client.getSpriteAtlas(new Identifier("block/fire_0"));
 		Tessellator tesselator = Tessellator.getInstance();
 		BufferBuilder vertexBufferBuilder = tesselator.getBuffer();
+		Matrix4f matrix4f = matrices.peek().getModel();
 		vertexBufferBuilder.begin(7, VertexFormats.POSITION_TEXTURE_COLOR);
-		vertexBufferBuilder.vertex(-0.5D, 0.0D, 0.0D).texture(barrelSprite.getMinU(), barrelSprite.getMaxV()).color(255, 255, 255, 255).next();
-		vertexBufferBuilder.vertex(0.5D, 0.0D, 0.0D).texture(barrelSprite.getMaxU(), barrelSprite.getMaxV()).color(255, 255, 255, 255).next();
-		vertexBufferBuilder.vertex(0.5D, -1.0D, 0.0D).texture(barrelSprite.getMaxU(), barrelSprite.getMinV()).color(255, 255, 255, 255).next();
-		vertexBufferBuilder.vertex(-0.5D, -1.0D, 0.0D).texture(barrelSprite.getMinU(), barrelSprite.getMinV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f, -0.5F, 0.0F, 0.0F).texture(barrelSprite.getMinU(), barrelSprite.getMaxV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f, .5F, 0.0F, 0.0F).texture(barrelSprite.getMaxU(), barrelSprite.getMaxV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f,.5F, -1.0F, 0.0F).texture(barrelSprite.getMaxU(), barrelSprite.getMinV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f,-0.5F, -1.0F, 0.0F).texture(barrelSprite.getMinU(), barrelSprite.getMinV()).color(255, 255, 255, 255).next();
 		vertexBufferBuilder.sortQuads(0.0F, -1.0F, 0.0F);
-		vertexBufferBuilder.vertex(-0.5D, 0.0D, 0.0D).texture(fireSprite.getMinU(), fireSprite.getMaxV()).color(255, 255, 255, 255).next();
-		vertexBufferBuilder.vertex(0.5D, 0.0D, 0.0D).texture(fireSprite.getMaxU(), fireSprite.getMaxV()).color(255, 255, 255, 255).next();
-		vertexBufferBuilder.vertex(0.5D, -1.0D, 0.0D).texture(fireSprite.getMaxU(), fireSprite.getMinV()).color(255, 255, 255, 255).next();
-		vertexBufferBuilder.vertex(-0.5D, -1.0D, 0.0D).texture(fireSprite.getMinU(), fireSprite.getMinV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f, -0.5F, 0.0F, 0.0F).texture(fireSprite.getMinU(), fireSprite.getMaxV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f, 0.5F, 0.0F, 0.0F).texture(fireSprite.getMaxU(), fireSprite.getMaxV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f, 0.5F, -1.0F, 0.0F).texture(fireSprite.getMaxU(), fireSprite.getMinV()).color(255, 255, 255, 255).next();
+		vertexBufferBuilder.vertex(matrix4f, -0.5F, -1.0F, 0.0F).texture(fireSprite.getMinU(), fireSprite.getMinV()).color(255, 255, 255, 255).next();
 		vertexBufferBuilder.sortQuads(0.0F, 0.0F, 0.0F);
 		tesselator.draw();
-		GlStateManager.popMatrix();
+		matrices.pop();
 	}
 }
